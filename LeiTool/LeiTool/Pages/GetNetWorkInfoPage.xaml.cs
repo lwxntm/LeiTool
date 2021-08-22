2296b1f58e7c1b88267112afa8a51952e892e04f
雷啸天 <canxuetian@outlook.com>
2021-08-22 21:30:05 +0800

    初步设计网络工具

---
+++ LeiTool/LeiTool/Pages/GetNetWorkInfoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LeiTool.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GetNetWorkInfoPage : ContentPage
+    {
+        public GetNetWorkInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var hc = new HttpClient();
+            var s1=await hc.GetStringAsync("https://api.ip.sb/geoip");
+            await this.DisplayAlert("网络信息", $"{s1}" ,"确认");
+        }
+    }
+}
